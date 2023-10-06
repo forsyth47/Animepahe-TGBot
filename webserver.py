f8795c497edf999e970f87b1334547088474e8ed
Reiji <88034696+forsyth47@users.noreply.github.com>
2023-10-07 05:23:43 +0530

    smol update -2

--- webserver.py
+++ webserver.py
@@ -13,6 +13,14 @@ def home():
       fline += line + "<br>"
   return fline
 
+@app.route('/animepahe.mp4')
+def proxy_link():
+    full_url = request.full_path
+    link = full_url.split('/animepahe.mp4?url=')[1]
+    headers = {'Content-Type': 'video/mp4'}
+    custom_response = Response("", status=302, headers=headers)
+    custom_response.headers['Location'] = link
+    return custom_response
 
 def run():
 
